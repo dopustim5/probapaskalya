5e53ccb61479ee5a628b87eb950d031bdf61b485
dopustim5 <116429935+dopustim5@users.noreply.github.com>
2022-10-22 12:38:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharpdlyaloxov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a= ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b= ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("x= ");
+            int x = int.Parse(Console.ReadLine());
+            int K = 0;
+            int c = 0;
+            if (a >= b)
+            {
+                Console.WriteLine("Измените числа");
+            }
+            else
+            {
+                x = a;
+            }
+            c = x / 2;
+            if
+            {
+                K = K + 1;
+            }
+            else
+            {
+                
+            }
+
+
+
+        }
+    }
+}
